8c0ac19477509590396fe6bf774e9fe7ad09d80e
Nicolas Barbier <49644477+Nicolas67500@users.noreply.github.com>
2020-03-13 14:09:47 +0100

    Add files via upload
    we added the house and the hitbox.

--- The Story Of Gerald.py
+++ The Story Of Gerald.py
@@ -15,7 +15,8 @@ import math
 # 18 Janvier. Nicolas: Animation du personnage faite et ajout de la musique d'ambiance et des bruits de pas
 # Problème à régler: le bruit des pas ne se fait qu'une seul fois et ne recommence pas quand j'appuie sur une touche
 
-W, H = 1280, 720  # Définition de l'écran
+# 4 Mars, mise en place de la hitbox du perso, de la maison.
+W, H = 1280, 598  # Définition de l'écran
 silencieux = 0  # Son coupé
 
 
@@ -23,10 +24,10 @@ silencieux = 0  # Son coupé
 pygame.init()
 
     # Ecran définition
-ecran = pygame.display.set_mode((W, H))
+ecran = pygame.display.set_mode((W, H), pygame.DOUBLEBUF | pygame.HWSURFACE| pygame.FULLSCREEN)
 
     # Icone -------
-icone = pygame.image.load("Images/icone.jpeg")
+icone = pygame.image.load("Images/icone.jpg").convert_alpha()
 pygame.display.set_icon(icone)
     # Titre -------
 pygame.display.set_caption("The Story of Gerald")
@@ -34,14 +35,18 @@ pygame.display.set_caption("The Story of Gerald")
     # Time -------------
 clock = pygame.time.Clock()
 
-accueil = pygame.image.load("Accueil.png")
+accueil = pygame.image.load("Accueil.png").convert_alpha()
+
+ROUGE = (255,0,0)
+
+# MENU DU JEU ------------------------
 
 def fade():
     fade = pygame.Surface((W, H))
     fade.fill((0, 0, 0))
     for alpha in range(0, 300, 2):
         fade.set_alpha(alpha)
-        ecran.blit(accueil, (0, 0))
+        ecran.blit(accueil, (0, -100))
         ecran.blit(fade, (0, 0))
         pygame.display.update()
 
@@ -52,9 +57,9 @@ def menu():
     pygame.mixer.music.fadeout(400)
     pygame.mixer.music.play()
     while run:
-        ecran.blit(accueil, (0, 0))
+        ecran.blit(accueil, (0, -100))
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (event.type == KEYDOWN and event.key == pygame.K_ESCAPE):
                 pygame.quit()
                 quit()
             if event.type == pygame.KEYDOWN:
@@ -63,7 +68,49 @@ def menu():
                     run = False
 
         pygame.display.flip()
+# -----------------------------------
 
+# Classe du personnage -----------------
+class personnage():
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+            self.nombredepas = 0
+            self.jumpCount = 8
+            self.jump = False
+            # Rect(left, top, width, height) -> Rect
+            self.hitbox = (self.x + 17, self.y + 16, 53, 85)
+
+
+        def mouvement(self,ImagePerso):
+            self.ImagePerso = ImagePerso
+            ecran.blit(ImagePerso[self.nombredepas // 3], (self.x, self.y))
+            self.nombredepas += 1
+
+            if self.nombredepas + 1 >= 45:
+                self.nombredepas = 0
+
+        def affichagehitbox(self, ecran):
+            # Rect(left, top, width, height) -> Rect
+            self.hitbox = (self.x + 17, self.y + 16, 53, 85)
+            pygame.draw.rect(ecran, (255, 0, 0), self.hitbox, 2)
+            # rect(surface, color, rect, width=0) -> Rect
+            # if width == 0, (default) fill the rectangle
+            # if width > 0, used for line thickness
+            # if width < 0, nothing will be drawn
+# Personnage ---------------
+
+# Personnage dans la fôret
+persomain = personnage(600, 430)
+# Personnage dans la maison
+persohouse = personnage(600,376)
+
+# test villageois
+persojean = personnage(400, 430)
+
+
+
+# Intérieur de la maison
 def main():
     # Sons pas ----------------
     sonpas = pygame.mixer.Sound("Sons/sonpas.wav")
@@ -81,65 +128,85 @@ def main():
     pygame.mixer.music.play()
 
     # Arrière plan --------------
-    fond = pygame.image.load("Images/fond.png").convert()
-    fond = pygame.transform.scale(fond, (W, H))
-    fondX = 0
-    fondX2 = fond.get_width()
-
-    # Premier plan (sol) ----------
-    sol = pygame.image.load("Images/sol.png").convert()
-    sol = pygame.transform.scale(sol, (1280, 167))
-    solX = 0
-    solX2 = sol.get_width()
+    fond = [pygame.image.load('Images/Fond/Fond' + str(k) + '.png').convert_alpha() for k in range(11)]
+    X = [0 for k in range(11)]
+    X2 = [928 for k in range(11)]
+    X3 = [1856 for k in range(11)]
 
     # Personnage --------------
-    droite_perso = [pygame.image.load('Images/R' + str(k) + '.png') for k in range(9)]
-    gauche_perso = [pygame.image.load('Images/L' + str(k) + '.png') for k in range(9)]
-    devant_perso = pygame.image.load('Images/standing.png')
+    droite_perso = [pygame.image.load('Images/droite' + str(k) + '.png').convert_alpha() for k in range(15)]
+    gauche_perso = [pygame.image.load('Images/gauche' + str(k) + '.png').convert_alpha() for k in range(15)]
+    devant_perso = pygame.image.load('Images/face0.png').convert_alpha()
 
-    # Classe du personnage -----------------
-    class personnage():
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
-            self.nombredepas = 0
+    def screenmain():
+        for k in range(11):
+            ecran.blit(fond[k], (X[k],-203))
+            ecran.blit(fond[k], (X2[k], -203))
+            ecran.blit(fond[k], (X3[k], -203))
 
-    def screen():
-        ecran.blit(fond, (fondX, 0))
-        ecran.blit(fond, (fondX2, 0))
-        ecran.blit(sol, (solX, 550))
-        ecran.blit(sol, (solX2, 550))
+
+        # Affichage du perso'
         ecran.blit(Jean, (persojean.x, persojean.y))
 
+        # Affichage de la hitbox du perso principal
+        persomain.affichagehitbox(ecran)
 
 
-    def defilement(ImagePerso):
 
-        # Déplacement perso-------------
-        ecran.blit(ImagePerso[perso.nombredepas // 3], (perso.x, perso.y))
-        perso.nombredepas += 1
+    def house():
+        house = True
+        ou = pygame.image.load("maison.png")
+        # Sons pas ----------------
 
-        if perso.nombredepas + 1 >= 27:
-            perso.nombredepas = 0
+        joue = silencieux
 
 
-    # Variable Jump
-    jump = False
-    jumpCount = 10
+        while house:
+            keys = pygame.key.get_pressed()
+
+
+            ecran.fill([0,0,0])
+            ecran.blit(ou, (240, 250))
+
+            # persohouse.affichagehitbox(ecran) permettant d'afficher la hitbox du perso principal
+            # bruit de pas intérieur à mettre
+
+            if keys[pygame.K_RIGHT] and persohouse.x <=1020:
+                persohouse.mouvement(droite_perso)
+                persohouse.x += 1.5
+
+
+            elif keys[pygame.K_LEFT]and persohouse.x >225:
+                persohouse.mouvement(gauche_perso)
+                persohouse.x -= 1.5
+
+
+            else:
+                ecran.blit(devant_perso, (persohouse.x, persohouse.y))
+            clock.tick(50)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.type == pygame.QUIT or (event.type == KEYDOWN and event.key == pygame.K_ESCAPE):
+                        pygame.quit()
+                        quit()
+                    if event.key == pygame.K_RETURN and persohouse.x < 230:
+                        house = False
+
+            pygame.display.update()
 
     # Boucle principale -----------------------------
     run = True
-
-    # Personnage ---------------
-    perso = personnage(600, 520)
-    persojean = personnage(1400, 480)
+    house()
 
     while run:
         keys = pygame.key.get_pressed()
 
-        clock.tick(32)
-        # Image afficher ----------------
-        screen()
+        clock.tick(60)
+        # Image affichage ----------------
+        screenmain()
 
         # Bruit de pas -----------
         if (keys[pygame.K_RIGHT] or keys[pygame.K_LEFT]):
@@ -152,71 +219,87 @@ def main():
         else:
             pygame.mixer.pause()
 
-        # deplacement du pnj
-        if keys[pygame.K_RIGHT]:
-            persojean.x -= 2
-        if keys[pygame.K_LEFT]:
-            persojean.x += 2
 
         # JUMP
-        if not (jump):
+        if not (persomain.jump):
             if keys[pygame.K_SPACE]:
-                jump = True
+                persomain.jump = True
         else:
-            if jumpCount >= -10:
+            if persomain.jumpCount >= -8:
                 neg = 1
-                if jumpCount < 0:
+                if persomain.jumpCount < 0:
                     neg = -1
-                perso.y -= (jumpCount ** 2) * 0.2 * neg
-                jumpCount -= 1
+                persomain.y -= (persomain.jumpCount ** 2) * 0.2 * neg
+                persomain.jumpCount -= 0.5
             else:
-                jump = False
-                jumpCount = 10
+                persomain.jump = False
+                persomain.jumpCount = 8
 
         if keys[pygame.K_RIGHT]:
-            defilement(droite_perso)
+            persomain.mouvement(droite_perso)
 
-            fondX -= 1; fondX2 -= 1; solX -= 2; solX2 -= 2
+            persojean.x -= 2.5
 
-            if fondX < fond.get_width() * -1:
-                fondX = fond.get_width()
-            if fondX2 < fond.get_width() * -1:
-                fondX2 = fond.get_width()
+            for k in range(11):
+                X[k] -= k / 4
+                X2[k] -= k / 4
+                X3[k] -= k / 4
 
-            if solX < sol.get_width() * -1:
-                solX = sol.get_width()
-            if solX2 < sol.get_width() * -1:
-                solX2 = sol.get_width()
+            for k in range(11):
+
+                if X[k] < -928:
+                    X[k] = 0
+                if X2[k] < 0:
+                    X2[k] = 928
+                if X3[k] < 928:
+                    X3[k] = 1856
 
 
 
 
         elif keys[pygame.K_LEFT]:
-            defilement(gauche_perso)
+            persomain.mouvement(gauche_perso)
 
-            fondX += 1; fondX2 += 1; solX += 2; solX2 += 2
+            persojean.x += 2.5
 
-            if fondX > fond.get_width():
-                fondX = fond.get_width() * -1
-            if fondX2 > fond.get_width():
-                fondX2 = fond.get_width() * -1
+            for k in range(11):
+                X[k] += k / 4
+                X2[k] += k / 4
+                X3[k] += k / 4
 
-            if solX > sol.get_width():
-                solX = sol.get_width() * -1
-            if solX2 > sol.get_width():
-                solX2 = sol.get_width() * -1
+            for k in range(11):
+
+                if X[k] > 0:
+                    X[k] = -928
+                if X2[k] > 928:
+                    X2[k] = 0
+                if X3[k] > 1856:
+                    X3[k] = 928
+
+
+        elif keys[pygame.K_q] and 300<persojean.x<600:
+            house()
+            pygame.display.flip()
 
 
         else:
-            ecran.blit(devant_perso, (perso.x, perso.y))
+            ecran.blit(devant_perso, (persomain.x, persomain.y))
+
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (event.type == KEYDOWN and event.key == pygame.K_ESCAPE):
                 run = False
+
+
+
         pygame.display.flip()
 
     pygame.quit()
+
     quit()
 
+
 menu()
+
 main()
+
